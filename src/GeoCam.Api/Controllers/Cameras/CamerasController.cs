d6218e887ae60269f8a01fb10220f90779dbcc67
Vanessa De Middelaer <vanessa.demiddelaer@craxit.be>
2020-01-10 21:57:01 +0100

    Quick and dirty querying due to time constraints

--- src/GeoCam.Api/Controllers/Cameras/CamerasController.cs
+++ src/GeoCam.Api/Controllers/Cameras/CamerasController.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using GeoCam.Api.DataAccess;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace GeoCam.Api.Controllers.Cameras
 {
 	[ApiController]
-	[ApiVersion("1")]
+	[ApiVersion("1.0")]
 	[Route("api/v{version:apiVersion}/[controller]")]
 	public class CamerasController : Controller
 	{
@@ -15,13 +20,25 @@ namespace GeoCam.Api.Controllers.Cameras
 			_logger = logger ?? throw new ArgumentNullException($"'{nameof(logger)}' cannot be null.");
 		}
 
+		/// <summary>
+		/// Returns all cameras that match the requested criteria
+		/// </summary>
+		/// <param name="dbContext"></param>
+		/// <param name="searchModel">Camera filter criteria</param>
+		/// <returns>List of cameras that match the parameters</returns>
 		[HttpPost("search")]
 		[Produces(System.Net.Mime.MediaTypeNames.Application.Json)]
 		[ProducesResponseType(typeof(void), StatusCodes.Status500InternalServerError)]
-		[ProducesResponseType(typeof(void), StatusCodes.Status501NotImplemented)]
-		public IActionResult Search([FromBody] Models.CameraSearchModel searchModel)
+		public async Task<IActionResult> Search([FromServices] GeoCamDbContext dbContext, [FromBody] Models.CameraSearchModel searchModel)
 		{
-			throw new NotImplementedException();
+			// *TODO: hacky - validate model + move to service + don't rely on name being the only criteria param + use automapper for the DTO
+			ICollection<Camera> cameras = null;
+			if (!String.IsNullOrEmpty(searchModel?.Name))
+				cameras = await dbContext.Cameras.Where(c => EF.Functions.Like(c.Name, $"%{searchModel.Name}%")).ToListAsync();
+			else
+				cameras = await dbContext.Cameras.ToListAsync();
+
+			return Ok(cameras.Select(c => new Models.CameraModel() { Name = c.Name, Longitude = c.Longitude, Latitude = c.Latitude }));
 		}
 
 		#region Fields
