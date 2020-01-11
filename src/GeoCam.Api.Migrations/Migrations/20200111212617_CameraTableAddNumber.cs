3347928fe3e386001b9b29731c677c1b8122f7dd
Vanessa De Middelaer <vanessa.demiddelaer@craxit.be>
2020-01-11 22:06:48 +0100

    Add missing migration

---
+++ src/GeoCam.Api.Migrations/Migrations/20200111212617_CameraTableAddNumber.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GeoCam.Api.Migrations.Migrations
+{
+    public partial class CameraTableAddNumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Number",
+                table: "Cameras",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Number",
+                table: "Cameras");
+        }
+    }
+}
